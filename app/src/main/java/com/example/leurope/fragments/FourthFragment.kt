31426bb4df5ee70aa31ea36d89bd50833d673f8d
Sergito772 <sergioleonhorea@gmail.com>
2023-03-08 00:45:01 +0100

    cuarto fragmento

--- app/src/main/java/com/example/leurope/fragments/FourthFragment.kt
+++ app/src/main/java/com/example/leurope/fragments/FourthFragment.kt
@@ -6,13 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.leurope.R
+import com.example.leurope.databinding.FourthFragmentBinding
 
 class FourthFragment: Fragment() {
+    private lateinit var binding: FourthFragmentBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fourth_fragment, container, false)
+        binding=FourthFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
